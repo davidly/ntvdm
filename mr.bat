0a0af0318ab6d592d990bbbb084afda09f5d56d3
davidly <davidly_lee@hotmail.com>
2023-05-22 13:14:36 -0700

    improve optimization for release build

--- mr.bat
+++ mr.bat
@@ -1,3 +1,3 @@
 @echo off
-cl /nologo ntvdm.cxx i8086.cxx /I. /EHsc /DNDEBUG /O2 /Oi /Fa /Qpar /Zi /link /OPT:REF user32.lib
+cl /nologo ntvdm.cxx i8086.cxx /I. /EHsc /DNDEBUG /Ot /Ox /Ob2 /Oi /Fa /Qpar /Zi /link /OPT:REF user32.lib
 
