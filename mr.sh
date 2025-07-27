7617b914f11c63cfc40e0edbe3b3d8561a125d15
davidly <davidly_lee@hotmail.com>
2025-07-27 12:43:03 -0700

    work around g++ compiler code generation bug with -O3 and -Ofast by using -O2

--- mr.sh
+++ mr.sh
@@ -1,1 +1,4 @@
-g++ -flto -ggdb -Ofast -fno-builtin -D NDEBUG -I . ntvdm.cxx i8086.cxx -o ntvdm -fopenmp -static
+# this compiler: g++ (Ubuntu 11.4.0-1ubuntu1~22.04) 11.4.0
+# generates bad code using -O3 and -Ofast. I've yet to get to the bottom of this compiler bug.
+
+g++ -flto -ggdb -O2 -fno-builtin -D NDEBUG -I . ntvdm.cxx i8086.cxx -o ntvdm -fopenmp -static
