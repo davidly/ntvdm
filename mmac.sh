073c6eed0bfdf918634c6f47a24d3b611e42da74
davidly <davidly_lee@hotmail.com>
2025-12-18 05:57:17 -0800

    -Ofast is deprecated with the macos c++ compiler

--- mmac.sh
+++ mmac.sh
@@ -1,1 +1,1 @@
-g++ -pthread -fsigned-char -ggdb -Ofast -fno-builtin -D DEBUG -I . ntvdm.cxx i8086.cxx -o ntvdm
+g++ -pthread -fsigned-char -ggdb -O3 -fno-builtin -D DEBUG -I . ntvdm.cxx i8086.cxx -o ntvdm
--- mmacr.sh
+++ mmacr.sh
@@ -1,1 +1,1 @@
-g++ -pthread -fsigned-char -ggdb -Ofast -fno-builtin -D NDEBUG -I . ntvdm.cxx i8086.cxx -o ntvdm
+g++ -pthread -fsigned-char -ggdb -O3 -fno-builtin -D NDEBUG -I . ntvdm.cxx i8086.cxx -o ntvdm
